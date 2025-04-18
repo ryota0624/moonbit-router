8d9913efdd571a843fb97242a8baa12d2a7c781b
ryota.suzuki <ryota.suzuki@10x.co.jp>
2025-04-19 00:16:25 +0900

    impl fetch api i/o

--- node/run_moonbit/src/index.ts
+++ node/run_moonbit/src/index.ts
@@ -1,17 +1,11 @@
-import { Hono } from 'hono'
 import * as M from "../../../target/js/release/build/hello";
-import { prettyJSON } from 'hono/pretty-json'
 
-const app = new Hono()
-
-app.use(prettyJSON())
-app.get('/', (c) => {
-  M.greeting_later((word) => {
-    console.log("Hello " + word);
-  })
-  return M.handle_greeting(c)
-})
-
-console.log("Server is running on http://localhost:3000")
-
-export default app
+Bun.serve({
+    fetch: async (req) => {
+        return new Promise<any>((resolve) => {
+            M.handle_greeting_callback(req, (response) => {
+                resolve(response);
+            });
+        })
+    },
+});
