e0f203e3504ceb53a7f3b63f117c58cfd83eec16
ryota.suzuki <ryota.suzuki@10x.co.jp>
2025-04-22 00:08:15 +0900

    fix function name

--- node/run_moonbit/src/index.ts
+++ node/run_moonbit/src/index.ts
@@ -3,7 +3,7 @@ import * as M from "../../../target/js/release/build/hello";
 Bun.serve({
     fetch: async (req) => {
         return new Promise<any>((resolve) => {
-            M.handle_greeting_callback(req, (response) => {
+            M.handle_fetch_api_request(req, (response) => {
                 resolve(response);
             });
         })
