6fd3cb17b8f55411973df12fda123841c94e91d6
ryota.suzuki <ryota.suzuki@10x.co.jp>
2025-04-27 01:11:23 +0900

    genericsでreq/resを扱わないようにする

--- node/run_moonbit/src/index.ts
+++ node/run_moonbit/src/index.ts
@@ -1,9 +1,10 @@
 import * as M from "../../../target/js/release/build/router";
 
+const router = M.handle_fetch_router();
 Bun.serve({
     fetch: async (req) => {
         return new Promise<any>((resolve) => {
-            M.handle_fetch_api_request(req, (response) => {
+            M.handle_request_by_router(router, req, (response) => {
                 resolve(response);
             }, (err) => {
                 console.error("Error:", err);
