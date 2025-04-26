26e0043b848879b947553caaf8d03c4b4708ec10
ryota.suzuki <ryota.suzuki@10x.co.jp>
2025-04-26 17:54:06 +0900

    use async fnc

--- node/run_moonbit/src/index.ts
+++ node/run_moonbit/src/index.ts
@@ -1,10 +1,13 @@
-import * as M from "../../../target/js/release/build/hello";
+import * as M from "../../../target/js/release/build/router";
 
 Bun.serve({
     fetch: async (req) => {
         return new Promise<any>((resolve) => {
             M.handle_fetch_api_request(req, (response) => {
                 resolve(response);
+            }, (err) => {
+                console.error("Error:", err);
+                resolve(new Response("Internal Server Error", { status: 500 }));
             });
         })
     },
