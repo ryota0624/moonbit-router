ff6defabf6f12187df79f70ab1e5bfb852b0c70e
ryota.suzuki <ryota.suzuki@10x.co.jp>
2025-05-25 10:19:10 +0900

    db問い合わせの実装

--- node/run_moonbit/src/index.ts
+++ node/run_moonbit/src/index.ts
@@ -1,10 +1,16 @@
-import * as M from "../../../target/js/release/build/router";
+import * as MyMoonBitLibrary from "../../../target/js/release/build/router";
 
-const router = M.handle_fetch_router();
+MyMoonBitLibrary.init_database((db) => {
+    console.log("Database initialized:", db);
+}, (err) => {
+    console.error("Error initializing database:", err);
+});
+
+const router = MyMoonBitLibrary.handle_fetch_router();
 Bun.serve({
     fetch: async (req) => {
         return new Promise<any>((resolve) => {
-            M.handle_request_by_router(router, req, (response) => {
+            MyMoonBitLibrary.handle_request_by_router(router, req, (response) => {
                 resolve(response);
             }, (err) => {
                 console.error("Error:", err);
